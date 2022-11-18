ff32dda15f833157eda6402bad6c618c60a2a7b1
Anastasiia <nmarkova273@gmail.com>
2022-11-18 16:51:54 +0300

    add start interface

--- qualifyingMasterWork/Form1.cs
+++ qualifyingMasterWork/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace qualifyingMasterWork
 {
-    public partial class Form1 : Form
+    public partial class Start : Form
     {
-        public Form1()
+        public Start()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
