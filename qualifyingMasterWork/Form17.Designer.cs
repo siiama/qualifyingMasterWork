7027a7fa7b741250d03e89973d9425426cbf0e74
Anastasiia <nmarkova273@gmail.com>
2022-11-20 16:08:32 +0300

    do form 17

---
+++ qualifyingMasterWork/Form17.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace qualifyingMasterWork
+{
+    partial class Form17
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ok = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.systemOfEquations = new System.Windows.Forms.RadioButton();
+            this.matrix = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(164, 262);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 75;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.UseWaitCursor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(39, 262);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 74;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // systemOfEquations
+            // 
+            this.systemOfEquations.AutoSize = true;
+            this.systemOfEquations.Location = new System.Drawing.Point(39, 183);
+            this.systemOfEquations.Name = "systemOfEquations";
+            this.systemOfEquations.Size = new System.Drawing.Size(147, 20);
+            this.systemOfEquations.TabIndex = 73;
+            this.systemOfEquations.Text = "system of equations";
+            this.systemOfEquations.UseVisualStyleBackColor = true;
+            // 
+            // matrix
+            // 
+            this.matrix.AutoSize = true;
+            this.matrix.Location = new System.Drawing.Point(39, 134);
+            this.matrix.Name = "matrix";
+            this.matrix.Size = new System.Drawing.Size(63, 20);
+            this.matrix.TabIndex = 71;
+            this.matrix.Text = "matrix";
+            this.matrix.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 16);
+            this.label1.TabIndex = 72;
+            this.label1.Text = "Please choose what kind of data do you want to get";
+            // 
+            // Form17
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.systemOfEquations);
+            this.Controls.Add(this.matrix);
+            this.Controls.Add(this.label1);
+            this.Name = "Form17";
+            this.Text = "Form17";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.RadioButton systemOfEquations;
+        private System.Windows.Forms.RadioButton matrix;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ qualifyingMasterWork/Form17.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qualifyingMasterWork
+{
+    public partial class Form17 : Form
+    {
+        Thread thread1, thread2, thread3;
+        public Form17()
+        {
+            InitializeComponent();
+        }
+        private void ok_Click(object sender, EventArgs e)
+        {
+            if (matrix.Checked)
+            {
+                thread2 = new Thread(openForm18);
+                thread2.SetApartmentState(ApartmentState.STA);
+                thread2.Start();
+                this.Close();
+            }
+            else if (systemOfEquations.Checked)
+            {
+                thread3 = new Thread(openForm19);
+                thread3.SetApartmentState(ApartmentState.STA);
+                thread3.Start();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please choose form of data");
+            }
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            thread1 = new Thread(openForm6);
+            thread1.SetApartmentState(ApartmentState.STA);
+            thread1.Start();
+            this.Close();
+        }
+        private void openForm6()
+        {
+            Application.Run(new Form06());
+        }
+        private void openForm18()
+        {
+            Application.Run(new Form18());
+        }
+        private void openForm19()
+        {
+            Application.Run(new Form19());
+        }
+    }
+}
