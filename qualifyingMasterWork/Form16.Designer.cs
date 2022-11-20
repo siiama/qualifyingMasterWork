8ee6c2aec75c6dace61e29a6422e4b02ed7b8e8a
Anastasiia <nmarkova273@gmail.com>
2022-11-20 16:51:18 +0300

    add form 15, 16

--- qualifyingMasterWork/Form16.Designer.cs
+++ qualifyingMasterWork/Form16.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.data = new System.Windows.Forms.DataGridView();
+            this.save = new System.Windows.Forms.Button();
+            this.ok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.back = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // data
+            // 
+            this.data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data.Location = new System.Drawing.Point(122, 97);
+            this.data.Name = "data";
+            this.data.RowHeadersWidth = 51;
+            this.data.RowTemplate.Height = 24;
+            this.data.Size = new System.Drawing.Size(240, 150);
+            this.data.TabIndex = 82;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(203, 286);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 81;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(339, 286);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 80;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.UseWaitCursor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 16);
+            this.label1.TabIndex = 79;
+            this.label1.Text = "Result: commutative diagram";
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(68, 286);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 78;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // Form16
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(482, 353);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.back);
+            this.Name = "Form16";
             this.Text = "Form16";
+            ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView data;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button back;
     }
 }
--- qualifyingMasterWork/Form16.cs
+++ qualifyingMasterWork/Form16.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,34 @@ namespace qualifyingMasterWork
 {
     public partial class Form16 : Form
     {
+        Thread thread1, thread2;
         public Form16()
         {
             InitializeComponent();
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            thread1 = new Thread(openForm14);
+            thread1.SetApartmentState(ApartmentState.STA);
+            thread1.Start();
+            this.Close();
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            thread2 = new Thread(openForm23);
+            thread2.SetApartmentState(ApartmentState.STA);
+            thread2.Start();
+            this.Close();
+        }
+        private void openForm14()
+        {
+            Application.Run(new Form14());
+        }
+        private void openForm23()
+        {
+            Application.Run(new Form23());
         }
     }
 }
